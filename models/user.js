56ed4bd860cded38762e2d4c666a1319cdcbc2ec
Krishna Kompalli <kompalligk@gmail.com>
2020-06-26 11:56:01 +0530

    modified pswd issue

--- models/user.js
+++ models/user.js
@@ -17,8 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     password:{
         type: String,
-        required: true,
-        minlength: 6
+        required: true
     },
 
     isVerified:{
